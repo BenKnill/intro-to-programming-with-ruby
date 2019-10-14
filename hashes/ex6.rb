3605f63684cdc868e0a08ff77d37a3991c18a432
Ben Knill <bknill@gmail.com>
2019-10-13 21:44:13 -0400

    completed hashes exercises

--- hashes/ex6.rb
+++ hashes/ex6.rb
@@ -4,3 +4,19 @@ words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
           'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
           'flow', 'neon']
 
+answer = {}
+
+words.each do |word|
+  alphabetized = word.split('').sort.join
+  if answer.has_key?(alphabetized)
+    answer[alphabetized].push(word)
+  else 
+    answer[alphabetized] = [word]
+  end 
+end 
+
+answer.each_value do |value|
+  puts 'these are anagrams'
+  puts value 
+end 
+
